591b91235f12cc353e403c6700b742534c0282eb
Eldio de Almeida Barbosa Neto <97411284+Eldio-Neto@users.noreply.github.com>
2024-01-04 11:16:38 +0000

    adicionando estilo no header

--- cinema_react/src/components/Header/index.js
+++ cinema_react/src/components/Header/index.js
@@ -4,7 +4,8 @@ import {Link} from 'react-router-dom';
 function Header (){
     return (
         <header>
-            
+            <Link className="logo" to="/">PrimeFlix</Link>
+            <Link className="favoritos" to="/favoritos">Meus Filmes</Link>
         </header>
     );
 } 
