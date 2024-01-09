cdeb15b3cf69f8139624a86e7c94cae5e2e3e02d
Eldio de Almeida Barbosa Neto <97411284+Eldio-Neto@users.noreply.github.com>
2024-01-09 00:22:06 +0000

    adicionando axios

--- cinema_react/src/pages/Filme/index.js
+++ cinema_react/src/pages/Filme/index.js
@@ -1,4 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+
 function Filme (){
+    const {id} = useParams();
+    const [filme, setFilme] = useState({});
+    const [loading, serLoading] = useState(true);
+    useEffect(() =>{
+         async function loadFilmes (){
+            await api.get(`/movie/${id}`,{
+                params:{
+                    api_key:'f46cf56386ef49f4ae57f91317162b84',
+                    language:'pt-br',
+                }
+            }).then(
+                (response)=>{
+                    setFilme()
+                }
+            ).catch(()=>{
+
+            });
+
+
+        };
+
+        loadFilmes();
+    },[])
+
+    if(loading){
+        div
+    }
+
     return(
         <div>
             <h1>Bem vindo aos Detalhes do Filme</h1>
--- cinema_react/src/pages/Home/index.js
+++ cinema_react/src/pages/Home/index.js
@@ -1,7 +1,54 @@
+import {useEffect, useState} from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+//URL API: movie/now_playing?api_key=f46cf56386ef49f4ae57f91317162b84&language=pt-br
+
 function Home (){
+    const [filmes, setFilmes] = useState();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilmes(){
+
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key:'f46cf56386ef49f4ae57f91317162b84',
+                    language:'pt-br',
+                    page:1
+                }
+            })
+
+            setFilmes(response.data.results.slice(0,10));
+            setLoading(false);
+        }
+
+        loadFilmes();
+    },[]);
+
+    if(loading){
+        return (
+            <div className='loading'>
+                <h2>Carregando...</h2>
+            </div>
+        );
+    }
+
     return(
-        <div>
-            <h1>Bem vindo ao Home</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+                 {filmes.map((filme)=>{
+                        return (
+                            <article key={filme.id} className="card"> 
+                                <strong>
+                                     {filme.title}
+                                </strong>
+                                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}/>
+                                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                            </article>
+                        );
+                 })}
+            </div>
         </div>
     )
 }
